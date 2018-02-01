dd88993b99ae8d96b6a9311a41f5b92edc5441c0
macRong <121071838@qq.com>
2018-02-01 14:20:50 +0800

    Add githooks project

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Author:macRong
+#Create Date:2017.11.19
+
+
+CURRENTPATH=`pwd` 
+PROJECTPATH=${CURRENTPATH%/*}
+TARGETPATH=$PROJECTPATH'/shell/limit_commit_temp.sh'
+PREPARECOMPATH=$PROJECTPATH'/.git/hooks/prepare-commit-msg'
+HOOKPATH=$PROJECTPATH'/.git/hooks'
+
+if [[ ! -e "$HOOKPATH" ]]; then
+	mkdir -p $HOOKPATH 
+fi
+
+
+cat $TARGETPATH > $PREPARECOMPATH
+chmod u+x $PREPARECOMPATH
+
+
+ 
+if [[ -x "$PREPARECOMPATH" ]]; then
+	echo "\033[32m 🍺 安装成功 ✅ \033[0m"
+else
+	echo "\033[31m 安装失败 ❌ \033[0m"
+fi
